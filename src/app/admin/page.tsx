e669ec104baa0c860d4f0d46d29396aa74e17235
RyannKim327 <rksesgundo123@gmail.com>
2026-06-30 16:44:53 +0800

    feat: initially created admin section

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,9 @@
+import Admin from "@/components/admin/hero";
+
+export default function () {
+  return (
+    <>
+      <Admin />
+    </>
+  )
+} 
---
+++ src/components/admin/hero.tsx
@@ -0,0 +1,9 @@
+export default function Admin() {
+  return (
+    <section
+      className="relative min-h-[90vh] flex items-center justify-start overflow-hidden pt-24"
+    >
+      Hello
+    </section>
+  )
+}
