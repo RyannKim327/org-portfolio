c096d6984ade1cce708eb8052737743548ec3475
RyannKim327 <rksesgundo123@gmail.com>
2026-07-02 14:32:43 +0800

    chore: header nav bar content removed in admin

--- src/components/admin/hero.tsx
+++
@@ -1,9 +0,0 @@
-export default function Admin() {
-  return (
-    <section
-      className="relative min-h-[90vh] flex items-center justify-start overflow-hidden pt-24"
-    >
-      hello
-    </section>
-  )
-}
---
+++ src/components/ui/sidebar-header.tsx
@@ -0,0 +1,9 @@
+export default function sidebarHeader() {
+  return (
+    <div className="flex relative overflow-hidden h-full w-full">
+      <div className="absolute z-10 w-[100px] h-[100px] bg-brand rotate-45 right-2 -bottom-8"></div>
+      <div className="absolute z-10 w-[100px] h-[100px] bg-[#0a0a0a]/75 border border-brand border-solid rotate-55 right-2 -bottom-8"></div>
+      <div className="absolute z-10 w-[100px] h-[100px] bg-brand rotate-75 right-2 -bottom-8"></div>
+    </div>
+  )
+}
