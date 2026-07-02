7332fd105441a469b215b5a4ce1491f363bde740
RyannKim327 <rksesgundo123@gmail.com>
2026-07-03 03:33:26 +0800

    feat: custom input and textarea

---
+++ src/components/ui/input.tsx
@@ -0,0 +1,17 @@
+interface inputProps extends React.InputHTMLAttributes<unknown> {
+  label: string
+}
+
+export function Input(props: inputProps) {
+  const label = props.label
+
+  return (
+    <div className="flex flex-col flex-1">
+      <span className="pl-3">{label}</span>
+      <input
+        {...props}
+        className={`${props.className ?? ""} p-2 bg-card-bg border border-solid border-card-border hover:border-brand focus:border-brand text-white outline-none rounded-lg transition-all ease-in-out delay-75`}
+      />
+    </div>
+  )
+}
