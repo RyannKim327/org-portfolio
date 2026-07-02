5e0482f775347c19fcb2752c404b8fe47ee03102
RyannKim327 <rksesgundo123@gmail.com>
2026-07-03 03:36:19 +0800

    feat: events page

--- src/app/admin/events/page.tsx
+++ src/app/admin/events/page.tsx
@@ -1,4 +1,7 @@
+import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 
 const events = [
   {
@@ -38,24 +41,40 @@ const events = [
 
 export default function Events() {
   return (
-    <div className="flex flex-row h-full w-full gap02">
-      {/* Form for making another event */}
-      <div className="h-full w-[calc(70%-0.5rem)]">
-        Form here
-      </div>
-      <div className="flex flex-col h-full w-[calc(30%-0.5rem)] gap-2 overflow-hidden overflow-y-auto scrollbar-track-transparent scrollbar-thumb-brand">
-        Lists here
+    <div className="flex flex-row h-full w-full gap-2">
 
+      {/* Form for making another event */}
+      <form className="flex flex-col h-full w-[calc(70%-0.5rem)] gap-2">
+        <div className="flex flex-col md:flex-row gap-2">
+          <Input label="Name" type="text" required />
+          <Input label="Type" type="text" required />
+        </div>
+
+        <div className="flex flex-col md:flex-row gap-2">
+          <Input label="Location" type="text" required />
+          <Input label="Schedule" type="datetime-local" required />
+        </div>
+
+        <div className="flex flex-col md:flex-row gap-2">
+          <Textarea className="resize-none h-[10rem]" label="Description" required />
+        </div>
+
+        {/* Action buttons */}
+        <div className="flex justify-end w-full gap-2">
+          <Button variant="outline" type="submit">Create event</Button>
+          <Button variant="solid" className="bg-error text-white hover:bg-error/75" type="submit">Clear</Button>
+        </div>
+      </form>
+      <div className="flex flex-col h-full w-[calc(30%-0.5rem)] gap-2 overflow-hidden overflow-y-auto scrollbar-track-transparent scrollbar-thumb-brand">
         {
           events.map((event) => {
             return (
-              <Card className="flex flex-col">
-                <div>
+              <Card className="flex flex-col gap-2 cursor-pointer">
+                <div className="flex flex-col">
                   <span>{event.title}</span>
                   <span className="text-xs">{event.date} - {event.location}</span>
                 </div>
                 <blockquote className="text-sm border-l-3 pl-2 border-l-brand border-l-solid">&emsp;{event.description}</blockquote>
-                <span>{ }</span>
               </Card>
             )
           })
