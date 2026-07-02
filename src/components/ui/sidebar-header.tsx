a7e7033ac1b7ff516e709224f9c8450ce5b3aa2c
RyannKim327 <rksesgundo123@gmail.com>
2026-07-03 03:34:48 +0800

    chore: cards in navigation header

--- src/components/ui/sidebar-header.tsx
+++ src/components/ui/sidebar-header.tsx
@@ -1,10 +1,10 @@
 export default function sidebarHeader() {
   return (
     <div className="flex relative overflow-hidden h-full w-full">
-      <div className="absolute z-10 w-[100px] h-[100px] bg-brand rotate-45 right-2 -bottom-8 border border-brand border-solid"></div>
-      <div className="absolute z-10 w-[100px] h-[100px] bg-brand-darker rotate-55 right-2 -bottom-8 border border-brand border-solid"></div>
-      <div className="absolute z-10 w-[100px] h-[100px] bg-brand rotate-75 right-2 -bottom-8 border border-brand border-solid "></div>
-      <div className="absolute z-10 w-[100px] h-[100px] bg-brand-darker rotate-90 right-2 -bottom-8 border border-brand border-solid"></div>
+      <div className="absolute z-10 w-[100px] h-[100px] bg-brand rotate-[30deg] right-2 -bottom-8 border border-brand-darker border-solid"></div>
+      <div className="absolute z-10 w-[100px] h-[100px] bg-brand-darker rotate-[45deg] right-2 -bottom-8 border border-brand border-solid"></div>
+      <div className="absolute z-10 w-[100px] h-[100px] bg-brand rotate-[60deg] right-2 -bottom-8 border border-brand-darker border-solid "></div>
+      <div className="absolute z-10 w-[100px] h-[100px] bg-brand-darker rotate-[75deg] right-2 -bottom-8 border border-brand border-solid"></div>
     </div>
   )
 }
