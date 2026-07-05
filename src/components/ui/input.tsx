229be720a7c8faf65aaddba8409727132d74d893
RyannKim327 <rksesgundo123@gmail.com>
2026-07-05 21:53:13 +0800

    chore: update textarea and input with * for required parameter

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -7,7 +7,7 @@ export function Input(props: inputProps) {
 
   return (
     <div className="flex flex-col flex-1">
-      <span className="pl-3">{label}</span>
+      <span className="pl-3">{label} <span className="text-red-500">{props.required ? "*" : ""}</span></span>
       <input
         {...props}
         className={`${props.className ?? ""} p-2 bg-card-bg border border-solid border-card-border hover:border-brand focus:border-brand text-white outline-none rounded-lg transition-all ease-in-out delay-75`}
--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -5,7 +5,7 @@ interface textareaProps extends React.TextareaHTMLAttributes<unknown> {
 export function Textarea(props: textareaProps) {
   return (
     <div className="flex flex-col flex-1">
-      <span className="pl-3">{props.label}</span>
+      <span className="pl-3">{props.label} <span className="text-red-500">{props.required ? "*" : ""}</span></span>
       <textarea
         {...props}
         className={`${props.className ?? ""} p-2 bg-card-bg border border-solid border-card-border hover:border-brand focus:border-brand text-white outline-none rounded-lg transition-all eas-in-out delay-75`}
